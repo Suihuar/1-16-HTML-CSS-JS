a362e4d667c9562ca66353683410224e12f9bfe9
suihua <2254579398@qq.com>
2025-02-02 12:45:28 +0800

    23.3 NPM

--- 23 NodeJS/2.2 Native Modules/index.js
+++ 23 NodeJS/2.2 Native Modules/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-fs.readFile("./message.txt",(err, data) => {
+
+fs.readFile("./message.txt","utf-8",(err, data) => {
     if (err) throw err;
     console.log(data);
-  }); 
+  });  
